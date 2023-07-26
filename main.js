6b275aba76b3cf4f68a4bd57e58223ae88901ac0
DonnyScript <donnyscript@gmail.com>
2023-07-25 20:58:07 -0400

    update yt

--- main.js
+++ main.js
@@ -77,15 +77,16 @@ client.on('messageCreate', async (message) => {
         return;
       } else if (message.content.startsWith('!byebyebtb')){
         const serverQueue = queues.get(message.guild.id);
-        let targetUser = await message.guild.members.fetch('265667476509949969');
+        // btb 265667476509949969
+        let targetUser = await message.guild.members.fetch('418235415665836033');
         if (message.member.voice.channel) {
             const connection = joinVoiceChannel({
               channelId: message.member.voice.channel.id,
               guildId: message.guild.id,
               adapterCreator: message.guild.voiceAdapterCreator,
             });
-            
-            const url = 'https://youtu.be/OKc-yQEiWjc'; 
+            message.channel.send(`${targetUser} BTB BIO-BOMB ACTIVATE`);
+            const url = 'https://youtu.be/phI_X8fwJx4'; 
             const stream = ytdl(url, { filter: 'audioonly' });
             const resource = createAudioResource(stream);
             const player = createAudioPlayer();
@@ -98,7 +99,6 @@ client.on('messageCreate', async (message) => {
           } else {
             message.reply('Join a voice channel first!');
           }
-        message.channel.send(`${targetUser} BTB BIO-BOMB ACTIVATE`);
       }
   });
   async function play(serverQueue) {
